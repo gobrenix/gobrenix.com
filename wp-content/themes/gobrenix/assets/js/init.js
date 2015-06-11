5cd5dc60def4f6b9fad9463a9967d75b94554c18
Jan Biasi <biasijan@gmail.com>
2015-06-12 01:41:04 +0200

    Work on style

--- wp-content/themes/gobrenix/assets/js/init.js
+++ wp-content/themes/gobrenix/assets/js/init.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
     if($(".button-collapse").length > 0) {
         $(".button-collapse").sideNav();
     }
+
+    if($('textarea').length > 0) {
+        $('textarea').addClass('materialize-textarea');
+    }
+
+    var makeButton = function() {
+        $(this).addClass('btn waves-effect');
+    };
+
+    $('input[type=submit]').each(makeButton);
+    $('.button').each(makeButton);
+    $('.tribe-events-button').each(makeButton);
 });
