72c2f9162b81da0542c09350dacb6db974b09e81
Jan Biasi <biasijan@gmail.com>
2015-06-12 02:01:48 +0200

    Minor updates

--- wp-content/themes/gobrenix/templates/artist.php
+++ wp-content/themes/gobrenix/templates/artist.php
@@ -8,14 +8,14 @@
  */
 
 get_header(); ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<?php while (have_posts()) : the_post(); ?>
-				<?php get_template_part('content', 'page'); ?>
-				<?php if (comments_open() || '0' != get_comments_number()) {
-						comments_template();
-				} ?>
-			<?php endwhile; ?>
-		</main>
-	</div>
+	<main role="main">
+		<div class="container artist-container">
+		<?php while (have_posts()) : the_post(); ?>
+			<?php get_template_part('content', 'page'); ?>
+			<?php if (comments_open() || '0' != get_comments_number()) {
+					comments_template();
+			} ?>
+		<?php endwhile; ?>
+		</div>
+	</main>
 <?php get_footer(); ?>
--- wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -1,2 +1,3 @@
 <?php exit(0); ?>
 {"user_id":1,"user_login":"admin","user_remoteaddr":"127.0.0.1","user_hostname":"localhost","user_lastlogin":"2015-06-11 23:37:22"}
+{"user_id":1,"user_login":"admin","user_remoteaddr":"127.0.0.1","user_hostname":"localhost","user_lastlogin":"2015-06-12 01:56:08"}
